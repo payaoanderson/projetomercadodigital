a724c4e95b37e651af423d191fb937bc636d1a1f
Anderson <andersonpayao018@gmail.com>
2024-08-27 15:42:47 -0300

    ...

--- resources/views/includes/cabeca.blade.php
+++ resources/views/includes/cabeca.blade.php
@@ -12,7 +12,7 @@
     <link
         href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,600;1,100&family=Poppins:wght@500&family=Vina+Sans&display=swap"
         rel="stylesheet">
-    <link rel="stylesheet" href="{{asset('./css/main.css')}}">
+    <link rel="stylesheet" href="{{asset('../css/main.css')}}">
 
 
     <title>Mercado Digital</title>
